a3d5207eeae4106f287702b0ab288cc040e2f592
markez92 <markez92@gmail.com>
2014-12-06 20:27:15 +0100

    move implementado

--- Programs/horses/horses.pl
+++ Programs/horses/horses.pl
@@ -1,4 +1,4 @@
-:-module(_,_).
+%:-module(_,_).
 
 % Caso de la casilla
 black.
@@ -26,20 +26,58 @@ salto_L_posible(N1, N2):-
 	(conexion_L(N1, N2);
 	conexion_L(N2, N1)).
 
-
-% Estados 
-
+% board/9, Ej: board(black, empty, black, empty, empty, empty, white, empty, white)
+% Estados InI
+board(Pos1, Pos2, Pos3, Pos4, empty, Pos6, Pos7, Pos8, Pos9):-
+	Pos1,
+	Pos2,
+	Pos3,
+	Pos4,
+	Pos6,
+	Pos7,
+	Pos8,
+	Pos9.
+	
 % TO-DO
 % solve/1, solve(Solution)
-% move/3, move(Color, Origin, Destination)
-% board/9, Ej: board(black, empty, black, empty, empty, empty, white, empty, white)
+
+solve(Solution):-
+	Estado = board(black, empty, black, empty, empty, empty, white, empty, white), %Incilizamos la estructura
+	length(Solution, Iteraciones).
+
 
 % move(Color, Origin, Destination)
-% representa la acción de mover un caballode color Color (variable que puede tomar uno 
+% representa la acción de mover un caballo de color Color (variable que puede tomar uno 
 % de los valores constantes black y white) desde la casilla Origin hasta la casilla 
 % Destination— que aplicadas secuencialmente desde el estado
 % inicial resuelven el problema propuesto
 
 % Ej: de llamada 
 
-%move(Color, Origin, Destination):-
+move(Color, Origin, Destination):-
+	conexion_L(Origin, Destination), %Movimientos posibles
+	Estado = board(black, empty, black, empty, empty, empty, white, empty, white), %PARA PROBAR move DE FORMA UNITARIA
+	Nuevo_Estado = board(_, _, _, _, empty, _, _, _, _),
+	arg(Origin, Estado, Color),  %Color en la posicion de origen
+	arg(Destination, Estado, empty), %Posicion destino libre
+	arg(Origin, Nuevo_Estado, empty), %Origen ahora libre
+	arg(Destination, Nuevo_Estado, Color), %Destino ahora con caballo Color
+	%Se actualizan el resto de posiciones, a excepcion de donde estaba el caballo y donde esta ahora.
+	actualiza_board(9, Origin, Destination, Estado, Nuevo_Estado). 
+
+actualiza_board(0, _, _, _, _).
+actualiza_board(N, Origen, Destino, Board, Nuevo_Board) :- 
+	(N = Origen;
+	N = Destino),
+	M is N-1, actualiza_board(M, Origen, Destino, Board, Nuevo_Board).
+actualiza_board(N, Origen, Destino, Board, Nuevo_Board) :- 
+	N>0,
+	N \== Origen,
+	N \== Destino,
+	arg(N, Board, Pos1),
+	arg(N, Nuevo_Board, Pos1),
+	M is N-1, 
+	actualiza_board(M, Origen, Destino, Board, Nuevo_Board).
+
+
+	
