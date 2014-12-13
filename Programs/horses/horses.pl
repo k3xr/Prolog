8c5a5bb4269431737c78db0acda374e8c29abe19
k3xr <k3ther@gmail.com>
2014-12-13 01:37:52 +0100

    Bonito

--- Programs/horses/horses.pl
+++ Programs/horses/horses.pl
@@ -8,9 +8,7 @@ empty.
 isColor(black).
 isColor(white).
 % Conexiones en L -> tenemos en cuenta que tenemos el siguiente grafo http://goo.gl/rETg7M
-% Estos son todos los movimientos que puede realizar un caballo. Para realizar la comprobación
-% de si son posibles estos movimientos creo que será necesario usar un or como: 
-% (conexion_L(1,6); conexion(6,1)) cuando queramos saber si podemos 
+% Movimientos que puede realizar un caballo.
 conexion_L(1,6).
 conexion_L(1,8).
 conexion_L(2,7).
@@ -21,14 +19,13 @@ conexion_L(4,9).
 conexion_L(6,7). 
 
 % salto_L_posible/2
-% Este predicado es verdadero (tablero 3x3 siempre que el caballo de encuentre en una de las 
+% Este predicado es verdadero (tablero 3x3) siempre que el caballo se encuentre en una de las 
 % dos posiciones y en su movimiento forme una L.
 salto_L_posible(N1, N2):-
 	(conexion_L(N1, N2);
 	conexion_L(N2, N1)).
 
 % board/9, Ej: board(black, empty, black, empty, empty, empty, white, empty, white)
-% Estados InI
 board(Pos1, Pos2, Pos3, Pos4, empty, Pos6, Pos7, Pos8, Pos9):-
 	Pos1,
 	Pos2,
@@ -38,14 +35,11 @@ board(Pos1, Pos2, Pos3, Pos4, empty, Pos6, Pos7, Pos8, Pos9):-
 	Pos7,
 	Pos8,
 	Pos9.
-	
-% TO-DO
-% solve/1, solve(Solution)
 
 solve(Solution):-
 	busca_sol(Solution,board(black, empty, black, empty, empty, empty, white, empty, white),[]).
 
-%Esta es la condición de parada. Que estado actual sea el que pone ahí en board	
+% Condición de parada.	
 busca_sol([],EstadoActual,_):-
 	EstadoActual = board(white, empty, white, empty, empty, empty, black, empty, black),!.
 	
@@ -64,7 +58,6 @@ busca_sol([H|Resto],EstadoActual,EstadosAnteriores):-
 	\+member(Nuevo_Estado,EstadosAnteriores),
 	append(EstadosAnteriores,[EstadoActual],NuevosEstadosAnteriores),
 	busca_sol(Resto,Nuevo_Estado,NuevosEstadosAnteriores).
-
 
 % move(Color, Origin, Destination)
 % representa la acción de mover un caballo
@@ -86,6 +79,4 @@ actualiza_board(N, Origen, Destino, Board, Nuevo_Board) :-
 	arg(N, Nuevo_Board, Pos1),
 	M is N-1, 
 	actualiza_board(M, Origen, Destino, Board, Nuevo_Board).
-
-
 	
